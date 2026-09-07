464a7f5f1dc397f773283550cf5b44406b0b9c47
root <root@DESKTOP-VGEBBR4.localdomain>
2026-09-07 10:15:23 +0100

    fix mariadb

--- srcs/requirements/mariadb/tools/scripte.sh
+++ srcs/requirements/mariadb/tools/scripte.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing DATABASE..."
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-
-    echo "Running bootstrap SQL..."
+	#bootstrap run mysqld in special initialization mode to setup SQL before mariadb starts.
     mysqld --user=mysql --bootstrap << EOF
-FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-    echo "Bootstrap done, $?"
-else
-    echo "Database already initialized"
 fi
-
 mkdir -p /run/mysqld
+#changes file or directory ownership
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
-
+#--console keeps its messages attached to the container console
 exec mysqld --user=mysql --console --port=3306
