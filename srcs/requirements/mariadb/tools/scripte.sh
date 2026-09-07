2c444eae5688c357947ec7fc3617e93f383ca29e
root <root@DESKTOP-VGEBBR4.localdomain>
2026-09-07 22:23:56 +0100

    inception kill me

--- srcs/requirements/mariadb/tools/scripte.sh
+++ srcs/requirements/mariadb/tools/scripte.sh
@@ -4,11 +4,12 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 	#bootstrap run mysqld in special initialization mode to setup SQL before mariadb starts.
     mysqld --user=mysql --bootstrap << EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
+	FLUSH PRIVILEGES;
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+		CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+		GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+	FLUSH PRIVILEGES;
 EOF
 fi
 mkdir -p /run/mysqld
@@ -16,4 +17,4 @@ mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 #--console keeps its messages attached to the container console
-exec mysqld --user=mysql --console --port=3306
+exec mysqld --user=mysql --console --port=3306 --bind-address=0.0.0.0
